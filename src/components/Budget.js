c04a7d3c76b9344e8583a334c0aa2916b3558a06
Lefteris <etroullouris@gmail.com>
2023-05-11 14:55:10 -0400

    Added input to budget with some functionality

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,33 @@
 
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, currency, dispatch } = useContext(AppContext);
+
+    const changeVal = (value) => {
+        if (parseInt(value) > 20000) {
+            alert("Budget cannot be over "+currency+"20000");
+        }
+        else {
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: value,
+            });
+        }
+
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}
+                <input
+                    required='required'
+                    type='number'
+                    id='cost'
+                    value={budget}
+                    step='10'
+                    style={{ marginLeft: '0.5rem', size: 10 }}
+                    onChange={(event) => changeVal(event.target.value)}>
+                </input></span>
         </div>
     );
 };
